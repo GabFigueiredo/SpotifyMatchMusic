e595f87983a6bb27d66baa76cd444d74200e51f3
GabFigueiredo <gabrielfandradee2@gmail.com>
2025-04-23 15:28:49 -0300

    removed secrets

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import spotipy
+import os
 from spotipy.oauth2 import SpotifyClientCredentials
 from google import genai
 
-# --- SPOTIFY ---
-client_id = '3e38317449e84311ba371c37c41feaaa'
-client_secret = '40e286bc493c4978864a525c900ae87e'
+from dotenv import load_dotenv
+load_dotenv()
 
-auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+auth_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 playlist_link = 'https://open.spotify.com/playlist/6DpZmGIT3RniGZjIPZmZlv?si=vGgfxGHbQK-Xq-iJ3W9LKw&pi=7e6oNPc7Sa2Nf'
@@ -43,8 +47,6 @@ Tópicos relacionados a esse tema:
 Para cada um dos tópicos acima, liste apenas os nomes das músicas da lista que se relacionam com ele.  
 Não explique, apenas liste.
 """
-
-GEMINI_API_KEY = 'AIzaSyC3r45qQIaYc3t0Wbw0rBJEm2pHHfE-a58' 
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 response = client.models.generate_content(
